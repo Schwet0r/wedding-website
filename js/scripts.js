43d37e13db2eada61a0d449d1f406a6683578ee3
Schwet0r <35267612+Schwet0r@users.noreply.github.com>
2023-06-20 23:35:22 +0200

    Update scripts.js
    Pulled from Original

--- js/scripts.js
+++ js/scripts.js
@@ -214,8 +214,11 @@ $(document).ready(function () {
 
         $('#alert-wrapper').html(alert_markup('info', '<strong>Just a sec!</strong> We are saving your details.'));
 
-
-            $.post('https://script.google.com/macros/s/AKfycbxGMjpnnlvsyRloMt4olocBADge5MIaQXkwFdkbySjNLLCN0ING5cogkwL3LKuZjq4K/exec', data)
+        if (MD5($('#invite_code').val()) !== 'b0e53b10c1f55ede516b240036b88f40'
+            && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
+            $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
+        } else {
+            $.post('https://script.google.com/macros/s/AKfycbyo0rEknln8LedEP3bkONsfOh776IR5lFidLhJFQ6jdvRiH4dKvHZmtoIybvnxpxYr2cA/exec', data)
                 .done(function (data) {
                     console.log(data);
                     if (data.result === "error") {
@@ -229,6 +232,7 @@ $(document).ready(function () {
                     console.log(data);
                     $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> There is some issue with the server. '));
                 });
+        }
     });
 
 });
